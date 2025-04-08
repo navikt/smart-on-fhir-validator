b418eb735018bd94297602300ce4a63d95098c92
Karl J. Overå <k@rl.run>
2025-04-08 10:49:06 +0200

    chore: fix lint and types

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
-import { type RouteConfig, route } from '@react-router/dev/routes'
+import { type RouteConfig, route, index } from '@react-router/dev/routes'
 
 export default [
-  route('/', 'routes/validation.tsx'),
+  index('routes/validation.tsx'),
   route('/launch', 'routes/launch/launch.tsx'),
   route('/fhir-tester', 'routes/fhir-tester.tsx'),
   route('/fhir-creator', 'routes/fhir-create-tester.tsx'),
