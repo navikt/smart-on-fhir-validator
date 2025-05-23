e6234ca7540d2b30a63badd803a227e153792dbd
Karl J. Overå <k@rl.run>
2025-05-23 08:47:48 +0200

    chore: base test for practitioner validation

--- src/components/validators/practitioner/PractitionerValidation.tsx
+++ src/components/validators/practitioner/PractitionerValidation.tsx
@@ -1,16 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import type { Practitioner } from 'fhir/r4'
 import Client from 'fhirclient/lib/Client'
-import { Validator } from '../../../validation/Validator'
 
 import { handleError } from '../../../utils/ErrorHandler'
-import { navRefs, simplifierRefs } from '../../../validation/common-refs'
 import { type Validation, validation } from '../../../validation/validation'
 import Spinner from '../../spinner/Spinner'
 import Validations from '../../validation-table/Validations'
 
-const hprSystemIdentifier = 'urn:oid:2.16.578.1.12.4.1.4.4'
-const herSystemIdentifier = 'urn:oid:2.16.578.1.12.4.1.2'
+import { validatePractitioner } from './validate-practitioner'
 
 export interface PractitionerValidationProps {
   readonly client: Client
@@ -50,78 +47,4 @@ export default function PractitionerValidation({ client }: PractitionerValidatio
       )}
     </div>
   )
-}
-
-function validatePractitioner(practitioner: Practitioner): Validation[] {
-  const validator = new Validator()
-
-  const meta = practitioner.meta
-
-  if (
-    !meta ||
-    !meta.profile ||
-    !meta.profile.includes('http://hl7.no/fhir/StructureDefinition/no-basis-Practitioner')
-  ) {
-    validator.error('The Practitioner must be of type no-basis-Practitioner', {
-      simplifier: simplifierRefs.noBasisPractitioner,
-      nav: navRefs.practitioner,
-    })
-  }
-
-  const norwegianHPRIdentifierSystem = practitioner.identifier?.find((id) => id.system === hprSystemIdentifier)
-  const norwegianHERIdentifierSystem = practitioner.identifier?.find((id) => id.system === herSystemIdentifier)
-
-  if (!norwegianHPRIdentifierSystem) {
-    validator.error(
-      `The Practitioner does not have a Norwegian Health Personnel Record number (HPR) from OID "${hprSystemIdentifier}"`,
-      { nav: navRefs.practitioner },
-    )
-  }
-
-  if (!norwegianHERIdentifierSystem) {
-    validator.warn(`The Practitioner does not have a Norwegian HER-ID (oid: ${herSystemIdentifier})`, {
-      nav: navRefs.practitioner,
-    })
-  }
-
-  const practitionerName = practitioner.name
-  if (!practitionerName || practitionerName.length === 0) {
-    validator.error(`The Practitioner does not have a name property`)
-  } else {
-    const humanName = practitionerName[0]
-    if (!humanName.family) {
-      validator.error('The Practitioner does not have a family name')
-    }
-    if (!humanName.given || humanName.given.length === 0) {
-      validator.error('The Practitioner does not have given name(s)')
-    }
-  }
-
-  const practitionerTelecom = practitioner.telecom
-  if (!practitionerTelecom || practitionerTelecom.length === 0) {
-    validator.error(`The Practitioner does not have a telecom property`, {
-      simplifier: simplifierRefs.telecom,
-      nav: navRefs.practitioner,
-    })
-  } else {
-    practitionerTelecom.forEach((telecom, index) => {
-      if (!telecom.system || !['phone', 'fax', 'email', 'pager', 'url', 'sms', 'other'].includes(telecom.system)) {
-        validator.error(
-          `The Practitioner content [${index}] does not have a telecom system: ${telecom.system ?? 'undefined'} `,
-          { simplifier: simplifierRefs.telecom, nav: navRefs.practitioner },
-        )
-      }
-      if (!telecom.value) {
-        validator.error(`The Practitioner content [${index}] does not have a telecom value`)
-      }
-      if (!telecom.use || !['home', 'work', 'temp', 'old', 'mobile'].includes(telecom.use)) {
-        validator.warn(
-          `The Practitioner content [${index}] does not have a telecom use: "${telecom.use ?? 'undefined'}"`,
-          { simplifier: simplifierRefs.telecom, nav: navRefs.practitioner },
-        )
-      }
-    })
-  }
-
-  return validator.build()
 }
---
+++ src/components/validators/practitioner/validate-practitioner.test.ts
@@ -0,0 +1,47 @@
+import type { Practitioner } from 'fhir/r4'
+import { describe, expect, it } from 'vitest'
+
+import { validatePractitioner } from './validate-practitioner'
+
+describe('validatePractitioner', () => {
+  it('should validate our own example structure', () => {
+    const example: Practitioner = {
+      resourceType: 'Practitioner',
+      meta: {
+        profile: ['http://hl7.no/fhir/StructureDefinition/no-basis-Practitioner'],
+      },
+      identifier: [
+        {
+          system: 'urn:oid:2.16.578.1.12.4.1.4.4',
+          value: 'hpr-nummer',
+        },
+        {
+          system: 'urn:oid:2.16.578.1.12.4.1.2',
+          value: 'her-id',
+        },
+      ],
+      name: [
+        {
+          family: 'Koman',
+          given: ['Magnar'],
+        },
+      ],
+      telecom: [
+        {
+          system: 'phone',
+          value: '12345678',
+          use: 'mobile',
+        },
+        {
+          system: 'email',
+          value: 'lege@epj.no',
+          use: 'work',
+        },
+      ],
+    }
+
+    const validations = validatePractitioner(example).filter((it) => ['WARN', 'ERROR'].includes(it.severity))
+
+    expect(validations).toEqual([])
+  })
+})
